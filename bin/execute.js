c67c4189952cb0de5be725309c6d7f779131eb77
xubo <xb451@163.com>
2020-10-14 20:05:32 +0800

    解决无css文件时打包的问题

--- bin/execute.js
+++ bin/execute.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
-var fs = require('fs');
-var ArgumentParser = require('argparse').ArgumentParser;
-var ai2font = require('../');
-var argsParser = new ArgumentParser({
+const fs = require('fs');
+const ArgumentParser = require('argparse').ArgumentParser;
+const ai2font = require('../');
+const argsParser = new ArgumentParser({
   version: require('../package.json').version,
   addHelp: true,
   description: 'SVG to font converter'
@@ -13,17 +13,13 @@ argsParser.addArgument(['-d', '--dir'], {
 argsParser.addArgument(['-t', '--target'], {
   help: '字体文件存放目录;最好不要和svg文件是同一个目录,或者是svg文件所在目录的子目录.'
 });
-argsParser.addArgument(['-D', '--deep'], {
-  help: '是否扫描-d指定目录的子目录,默认不扫描',
-  action: 'storeTrue'
-});
 argsParser.addArgument(['-f', '--font'], {
   help: '指定字体名称'
 });
-argsParser.addArgument(['-s', '--separate'], {
-  help: '指定svg文件名的分隔符,默认为~, 暂不支持此参数'
+argsParser.addArgument(['-c', '--css'], {
+  help: '设置css文件存放目录, svgs目录的相对目录'
 });
-var args = argsParser.parseArgs();
+const args = argsParser.parseArgs();
 if (args.dir) {
   fs.stat(args.dir, function(err, stat) {
     if (err) {
@@ -39,3 +35,4 @@ if (args.dir) {
 } else {
   ai2font(args);
 }
+
--- index.js
+++ index.js
@@ -1,93 +1,67 @@
-var readDir = require('./lib/readDir');
-var fs = require('fs');
-var path = require('path');
-var svg2ttf = require('svg2ttf');
-var ttf2eot = require('ttf2eot');
-var ttf2woff = require('ttf2woff');
-var svgicons2svgfont = require('svgicons2svgfont');
-var generateCss = require('./lib/generatecss');
-var generateHtml = require('./lib/generateHtml');
-var config = require('./lib/config');
-var writeFile = require('./lib/writeFile');
-var TTFStream = require('./lib/svg2ttfstream');
+const R = require('ramda');
+const fs = require('fs');
+const path = require('path');
+const ttf2eot = require('ttf2eot');
+const ttf2woff = require('ttf2woff');
+const svgicons2svgfont = require('svgicons2svgfont');
 
-module.exports = function(options) {
-  options = options || {};
-  var isDeep = options.deep;
-  var cdp = options.dir || process.cwd();
-  readDir({
-    dirpath: cdp,
-    callback: function(err, list) {
-      if (!list.length) {
-        return;
-      }
-      var targetDir = options.target || config.getTargetDir(cdp);
-      var webPath = config.getWebPath(cdp, targetDir);
-      var fontName = options.font || config.getFontName();
-      var separate = options.separate || '~';
-      var cssFilePath = config.getCssPath(cdp);
-      var htmlFilePath = config.getHtmlPath(cdp);
-      var htmlWebUrl = config.getHtmlWebUrl(cdp);
-      var cssWebUrl = config.getCssWebUrl(cdp);
-      var mark = config.getMark(cdp);
-      var fontFilePath = path.join(targetDir, fontName+'.svg');
-      var ttfFilePath = path.join(targetDir, fontName+'.ttf');
-      var woffFilePath = path.join(targetDir, fontName+'.woff');
-      var eotFilePath = path.join(targetDir, fontName+'.eot');
-      writeFile.mkdirs(path.dirname(fontFilePath), function() {
-        var svgStream = svgicons2svgfont(list, {
-          fontName: fontName,
-          normalize: true
-        });
-        svgStream.pipe(fs.createWriteStream(fontFilePath));
-        var ttfStream = svgStream.pipe(new TTFStream({fp: ttfFilePath}));
-        ttfStream.on('end', function(data) {
-          console.log('ttf created.');
-          var ttfcontent = new Uint8Array(data);
-          var woffcontent = new Buffer(ttf2woff(ttfcontent).buffer);
-          var eotcontent = new Buffer(ttf2eot(ttfcontent).buffer);
-          writeFile(woffFilePath , woffcontent, 'utf-8', function() {
-            if (!err) {
-              console.log('woff created.');
-            }
-          });
-          writeFile(eotFilePath , eotcontent, 'utf-8', function() {
-            if (!err) {
-              console.log('eot created.');
-            }
-          });
-        });
-      });
+const glyphs = require('./lib/readFile');
+const generateCss = require('./lib/generatecss');
+const writeFile = require('./lib/writeFile');
+const TTFStream = require('./lib/svg2ttfstream');
 
-      fs.readFile(cssFilePath, 'utf-8', function(err, data) {
-        var cssText = generateCss(fontName, webPath, list, mark);
-        if (err) {
-          data = cssText;
-        } else {
-          var reg = new RegExp('\\/\\*font\\-icon\\-' + (mark?mark+'\\-':'') + 'start\\*\\/[\\s\\S]*\\/\\*font\\-icon\\-' + (mark?mark+'\\-':'') + 'end\\*\\/','i');
-          if (!data) {
-            data = cssText;
-          } else if (reg.test(data)) {
-            data = data.replace(reg, cssText);
-          } else {
-            data = data + '\n' + cssText;
-          }
-        }
-        writeFile(cssFilePath, data, 'utf-8', function(err) {
-          console.log('css created.' + cssFilePath);
-        });
-      });
-      writeFile(htmlFilePath, generateHtml(cssWebUrl, list),'utf-8', function(err){
-        if (err) {
-          console.log(err);
-          return ;
-        }
-        console.log('html created');
-        var c = require('child_process');
-        c.exec('start ' + htmlWebUrl);
-        console.log(htmlWebUrl);
-      });
-    },
-    isDeep: isDeep
+const fontPath = (dir, fontName) => ext => path.join(dir, `${fontName}.${ext}`);
+
+const cssFile = R.curry((cssFilePath, list) => {
+  list && list.length && fs.readFile(cssFilePath, 'utf-8', function (err, data) {
+    let cssText = generateCss(list);
+    const reg = /\/\*font-icon-start\*\/[\s\S]*\/\*font-icon-end\*\//i;
+    if (reg.test(data)) {
+      cssText = data.replace(reg, cssText);
+    } else if (data) {
+      cssText = `${data}\n${cssText}`;
+    }
+    writeFile(cssFilePath, cssText, 'utf-8', function (err) {
+      console.log(`css created.${cssFilePath}`);
+    });
   });
+});
+const mapping = R.reduce((map, { name, codepoint }) => {
+  map[name] = codepoint.toString(16);
+  return map;
+}, {});
+const svgFontStream = fontName => list => list && list.length && svgicons2svgfont(list, { fontName, normalize: true });
+const generateTTFFont = ttfFilePath => svgStream => svgStream.pipe(new TTFStream({ fp: ttfFilePath }));
+const generateSVGFont = fontFilePath => svgStream => svgStream.pipe(fs.createWriteStream(fontFilePath));
+const woffBuffer = ttfContent => new Buffer(ttf2woff(ttfContent).buffer);
+const eotBuffer = ttfContent => new Buffer(ttf2eot(ttfContent).buffer);
+const logger = msg => err => (err ? console.error(err) : console.log(msg));
+const writeFontFile = R.curry((filePath, fileContent) => writeFile(filePath, fileContent, 'utf8', logger(`${filePath} Done.`)));
+const ttfComplete = callback => ttfStream => ttfStream.on('end', data => callback(new Uint8Array(data)));
+const creatDir = R.curry((dir, callback) => writeFile.mkdirs(dir, callback));
+module.exports = function ({ dir, target, font, css } = {}) {
+  const cdp = dir || process.cwd();
+  const targetDir = target || path.resolve(cdp, '../');
+  const fontName = font || 'h5icon';
+  const cssFilePath = path.resolve(targetDir, css || 'font.css');
+  const filePath = fontPath(targetDir, fontName);
+  const writeCssFile = cssFile(cssFilePath);
+  const mappingFile = writeFontFile(path.resolve(cdp, 'mapping.json'));
+  const writeMappingFile = R.compose(mappingFile, JSON.stringify, mapping);
+  const generateWOFFFont = R.compose(writeFontFile(filePath('woff')), woffBuffer);
+  const generateEOTFont = R.compose(writeFontFile(filePath('eot')), eotBuffer);
+  const createFontFile = R.converge(ttfComplete(ttf => {
+    generateWOFFFont(ttf);
+    generateEOTFont(ttf);
+  }), [generateTTFFont(filePath('ttf')), generateSVGFont(filePath('svg'))]);
+  const createFont = R.converge(
+    createFontFile,
+    [
+      svgFontStream(fontName),
+      writeCssFile,
+      writeMappingFile
+    ]
+  );
+  const svg2font = R.compose(createFont, glyphs);
+  creatDir(targetDir, () => svg2font(cdp));
 };
